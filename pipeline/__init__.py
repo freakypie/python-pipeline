287e57f6b7550b64c281d3937ec3ffd51346af32
John Leith <jleith@velocitywebworks.com>
2015-09-04 14:03:43 -0600

    packagified

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,1 +1,4 @@
 from .main import Pipeline
+
+
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='pipeline',
+    version=__import__('pipeline').__version__,
+    author='John Leith',
+    author_email='leith.john@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/freakypie/python-pipline',
+    zip_safe=False,
+)
